f6e3a790536fb20d473565e98ad927406c52191e
Adham Gohar <125025049+Adham-Gohar@users.noreply.github.com>
2024-01-08 21:29:10 +0200

    Add files via upload

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'Adham-Gohar', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,59 +11,45 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
-    instagram: '',
-    youtube: '', // example: 'pewdiepie'
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    linkedin: 'adham-gohar',
+    //twitter: '',
+ //   facebook: '',
+ //   instagram: '',
+ //   youtube: '', // example: 'pewdiepie'
+  //  dribbble: '',
+  //  behance: '',
+  //  medium: '',
+ //   dev: '',
+  //  stackoverflow: '', // example: '1/jeff-atwood'
+  //  skype: '',
+  //  telegram: '',
+ //   website: '',
+    phone: '+201211163974',
+    email: 'agohar21@aucegypt.edu',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/Abdallah-Afifi/Resume/raw/main/Abdallah_Afifi___Resume.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'C', 'C++', 'Python', 'Java', 'Kotlin', 'Dart', 'Flutter', 'React Native', 'JavaScript', 'HTML', 'CSS', 'PHP', 'Node.js',
   ],
-  experiences: [
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
-    },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
-    },
-  ],
+//  experiences: [
+  //  {
+    //  company: 'Company Name',
+    //  position: 'Position',
+    //  from: 'September 2021',
+    //  to: 'Present',
+  //    companyLink: 'https://example.com',
+  //  },
+  //  {
+   //   company: 'Company Name',
+    //  position: 'Position',
+   //   from: 'July 2019',
+   //   to: 'August 2021',
+   //   companyLink: 'https://example.com',
+  //  },
+ // ],
   /* certifications: [
     {
       name: 'Lorem ipsum',
@@ -74,42 +60,41 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'The American University in Cairo',
+      degree: 'Bachelor\'s of Computer Science',
+      from: '2022',
+      to: '2026',
+      Award: 'USAID-AUC Merit Award',
+      coursework: 'AUC: Fundamentals of Computing I, Fundamentals of Computing II, Applied Data Structures, Discrete Mathematics,  Linear Algebra; Harvard: CS50 Introduction to Computer Science; MIT: Introduction to Computer Science and Programming Using Python, Mathematics for Computer Science, Introduction To Algorithms; UMich: Python for Everybody, Problem-Solving using Computational Thinking; UCSD: Mathematical Thinking in Computer Science'
+ 
+
     },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
+
   ],
 
   // To hide the `My Projects` section, keep it empty.
-  externalProjects: [
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-  ],
+ // externalProjects: [
+   // {
+     // title: 'Project Name',
+     // description:
+      //  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+     // imageUrl: 'https://via.placeholder.com/250x250',
+    //  link: 'https://example.com',
+   // },
+   // {
+    //  title: 'Project Name',
+    //  description:
+   //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+   //   imageUrl: 'https://via.placeholder.com/250x250',
+    //  link: 'https://example.com',
+ //   },
+ // ],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
+  //blog: {
+   // source: 'dev', // medium | dev
+  //  username: 'arifszn', // to hide blog section, keep it empty
+   // limit: 2, // How many posts to display. Max is 10.
+  //},
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -119,15 +104,15 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
+    respectPrefersColorScheme: true,
 
     // Hide the ring in Profile picture
     hideAvatarRing: false,
@@ -136,54 +121,47 @@ const config = {
     themes: [
       'light',
       'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'procyon',
+   //   'cupcake',
+    //  'bumblebee',
+    //  'emerald',
+    //  'corporate',
+    //  'synthwave',
+    //  'retro',
+    //  'cyberpunk',
+    //  'valentine',
+    //  'halloween',
+    //  'garden',
+    //  'forest',
+     // 'aqua',
+    //  'lofi',
+    //  'pastel',
+    //  'fantasy',
+     // 'wireframe',
+     // 'black',
+     // 'luxury',
+    //  'dracula',
+     // 'cmyk',
+     // 'autumn',
+    //  'business',
+     // 'acid',
+    //  'lemonade',
+    //  'night',
+    //  'coffee',
+    //  'winter',
+    //  'procyon',
     ],
 
     // Custom theme
-    customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
-    },
+   // customTheme: {
+   //   primary: '#fc055b',
+   //   secondary: '#219aaf',
+    //  accent: '#e8d03a',
+    //  neutral: '#2A2730',
+    //  'base-100': '#E3E3ED',
+    //  '--rounded-box': '3rem',
+    //  '--rounded-btn': '3rem',
+   // },
   },
-
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
 };
 
 export default config;
